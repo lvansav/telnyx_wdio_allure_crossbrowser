7a2efd6c882441876e654cc141e29f92ddcae157
lvansav <vansav.ka@gmail.com>
2022-09-23 13:35:39 +0300

    Added new test and correct timeout in the conf file

--- test/specs/sip.trunking.sign.up.js
+++ test/specs/sip.trunking.sign.up.js
@@ -1,3 +1,5 @@
+const { faker } = require('@faker-js/faker')
+
 describe('Registration and contact support test suite for the SIP Trunking page', () => {
     beforeEach( async () => {
         const headerHidenLink = await $('header div>span>a')
@@ -7,8 +9,32 @@ describe('Registration and contact support test suite for the SIP Trunking page'
         const sipTrunking = await $('header [href*="sip-trunks"]')
         await sipTrunking.click()
     })
+    
     it('Should sign up from the SIP Trunking product page', async () => {
+        const getFreeBtn = await $('div:nth-child(4) [href="/sign-up"]')
+        await getFreeBtn.click()
+        const randEmail = faker.internet.email()
         
-        await browser.pause(4000)
+        const emailInp = await $('#email')
+        const nameInp = await $('#full_name')
+        const passInp = await $('#password')
+        const termsBox = await $('[aria-labelledby="terms-label"]')
+
+        await emailInp.addValue(randEmail)
+        await nameInp.addValue(faker.name.fullName())
+        await passInp.addValue(faker.internet.password(30, false, /[!-}]/, '!1So'))
+        await termsBox.click()
+
+        const createAccBtn = await $('[type="submit"]')
+        await createAccBtn.click()
+        await createAccBtn.click()
+
+        await expect(browser).toHaveUrlContaining('/sign-up/verify-email/f')
+        
+        const verifyEmail = await $('p>strong')
+        await expect(verifyEmail).toHaveText(randEmail)
+
+        const resendLink = await $('main button')
+        await expect(resendLink).toBeDisplayed()
     });
 })
