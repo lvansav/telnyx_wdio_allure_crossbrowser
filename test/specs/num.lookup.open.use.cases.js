f4e4a7c5212fe3d00e8cbfac5cbe970569d0e886
lvansav <vansav.ka@gmail.com>
2022-09-24 16:42:32 +0300

    Add two tests to the num.lookup.use.cases.js spec

--- test/specs/num.lookup.open.use.cases.js
+++ test/specs/num.lookup.open.use.cases.js
@@ -29,4 +29,46 @@ describe('Opening use cases test suite for the Number Lookup product page', () =
         await expect(getEBookBtn).toBeClickable()
         await expect(signUpFor10Btn).toBeClickable()
     });
+
+    it('Should to open the Two-Factor Authentication use case page by clicking the\
+      Two-Factor Authentication use case block in the Number Lookup product page', async () => {
+        const twoFactAuthUseCase = await $("[href*='/two-factor-authentication']")
+        await twoFactAuthUseCase.click()
+
+        const twoFactAuthUrl = /use-cases\/two-factor-authentication$/
+        const twoFactPageTitle = await $('h1 span')
+        const twoFactTitleTxt = 'Two Factor Authentication'
+        const flexPrivatNetSubtitle = await $('header h2:nth-child(2)')
+        const flexPrivateSubtitleTxt = 'Flexible APIs over a private IP network'
+        const tryItFreeBtn = await $('div:nth-child(7) [href="/sign-up"]')
+
+        await expect(browser).toHaveUrlContaining(twoFactAuthUrl)
+        await expect(twoFactPageTitle).toBeDisplayed()
+        await expect(twoFactPageTitle).toHaveText(twoFactTitleTxt)
+        await expect(flexPrivatNetSubtitle).toBeDisplayed()
+        await expect(flexPrivatNetSubtitle).toHaveText(flexPrivateSubtitleTxt)
+        await expect(tryItFreeBtn).toBeClickable()
+    });
+
+    it('Should to open the SMS Marketing use case page by clicking the\
+    SMS Marketing use case block in the Number Lookup product page', async () => {
+        const smsMarketingUseCase = await $('[href*="/sms-marketing"]')
+        await smsMarketingUseCase.click()
+
+        const smsMarketCaseUrl = /use-cases\/sms-marketing$/
+        const smsMarketPageTitle = await $('h1>span')
+        const smsMarketTitleTxt = 'SMS Marketing Software API'
+        const smsMarketPlatformSubtitle = await $('header>h2:nth-child(2)')
+        const smsMarketPlatformSubtitleTxt = 'SMS Marketing Platform that Delivers'
+        const seeAllProdLink = await $('main [href="/products"]')
+        const tryItFreeBtn = await $('div:nth-child(7) [href="/sign-up"]')
+
+        await expect(browser).toHaveUrlContaining(smsMarketCaseUrl)
+        await expect(smsMarketPageTitle).toBeDisplayed()
+        await expect(smsMarketPageTitle).toHaveText(smsMarketTitleTxt)
+        await expect(smsMarketPlatformSubtitle).toBeDisplayed()
+        await expect(smsMarketPlatformSubtitle).toHaveText(smsMarketPlatformSubtitleTxt)
+        await expect(seeAllProdLink).toBeClickable()
+        await expect(tryItFreeBtn).toBeClickable()
+    })
 })
