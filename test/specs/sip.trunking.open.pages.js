a753b0c0197efb95f1c0facba23015957358ce26
lvansav <vansav.ka@gmail.com>
2022-09-24 13:59:48 +0300

    Add test to the open pages suite

--- test/specs/sip.trunking.open.pages.js
+++ test/specs/sip.trunking.open.pages.js
@@ -51,5 +51,34 @@ describe('Going to another page test suite for the SIP Trunking page', () => {
 
         
         await expect(await $('//div/form/../../div[4]')).toHaveText("Thank you. We'll email you pricing right away!")
-      });
+    });
+
+    it('Should to Open the "What are the support hours?" article from\
+      the SIP Trunking product page', async () => {
+        const unparalSupportLink = await $('[href*="what-are-the-support-hours"]')
+        await unparalSupportLink.click()
+
+        const articleUrl = /what-are-the-support-hours$/
+        const articleTitle = await $('h1.t__h1')
+        const articleTitleTxt = 'What are the support hours?'
+        const articleAuthor = await $('.avatar span')
+        const articleAuthorTxt = /Telnyx Sales/
+        const supportEmail = await $('.intercom-content-link')
+        const supportEmailTxt = 'support@telnyx.com'
+        const reactionPicker = await $('.intercom-reaction-picker')
+        const reactionsEmoji = await $$('.intercom-reaction')
+        
+        await expect(browser).toHaveUrlContaining(articleUrl)
+        await expect(articleTitle).toBeDisplayed()
+        await expect(articleTitle).toHaveText(articleTitleTxt)
+        await expect(articleAuthor).toBeDisplayed()
+        await expect(articleAuthor).toHaveText(articleAuthorTxt)
+        await expect(supportEmail).toBeDisplayed()
+        await expect(supportEmail).toHaveText(supportEmailTxt)
+        await expect(reactionPicker).toBeDisplayed()
+        reactionsEmoji.forEach(async emoji => {
+            await emoji.click()
+            await expect(emoji).toHaveAttrContaining('class', 'intercom-reaction-picker-reaction-selected')
+        })
+    })
 })
