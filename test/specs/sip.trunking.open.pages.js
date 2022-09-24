34cad22d7edf968de6701956c9384608b46ff42e
lvansav <vansav.ka@gmail.com>
2022-09-24 13:24:15 +0300

    Add test to the open pages suite

--- test/specs/sip.trunking.open.pages.js
+++ test/specs/sip.trunking.open.pages.js
@@ -1,3 +1,5 @@
+const { faker } = require('@faker-js/faker')
+
 describe('Going to another page test suite for the SIP Trunking page', () => {
     beforeEach( async () => {
         const headerHidenLink = await $('header div>span>a')
@@ -28,4 +30,26 @@ describe('Going to another page test suite for the SIP Trunking page', () => {
         await expect(pricingBtn).toBeClickable()
         await expect(startFreeTrialBtn).toBeClickable()
     });
+
+    it('Should to open the Elastic SIP Trunking price page from the Sip \
+      Trunking product page and send the pricelist csv file to the random email', async () => {
+        const seePricing = await $('main [href="/pricing/elastic-sip"]')
+        await seePricing.click()
+        
+        const downloadForm = await $('#pricing_download_form')
+        await downloadForm.scrollIntoView()
+
+        const firstNameInp = await $('form>div:nth-child(1) input')
+        const lastNameInp = await $('form>div:nth-child(2) input')
+        const emailInp = await $('form>div:nth-child(3) input')
+        const downloadBtn = await $('#pricing_download_form button')
+
+        await firstNameInp.addValue(faker.name.firstName())
+        await lastNameInp.addValue(faker.name.lastName())
+        await emailInp.addValue(faker.internet.email())
+        await downloadBtn.click()
+
+        
+        await expect(await $('//div/form/../../div[4]')).toHaveText("Thank you. We'll email you pricing right away!")
+      });
 })
