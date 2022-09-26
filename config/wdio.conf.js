7db877014df3af8a4d3ce42eb987b95cb66dc73f
lvansav <vansav.ka@gmail.com>
2022-09-26 21:15:01 +0300

    Increase the timeout range

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -31,7 +31,7 @@ exports.config = {
     logLevel: 'info',
     bail: 0,
     baseUrl: 'http://localhost',
-    waitforTimeout: 120000,
+    waitforTimeout: 180000,
     connectionRetryTimeout: 180000,
     connectionRetryCount: 3,
     services: ['chromedriver','geckodriver'],
@@ -44,7 +44,7 @@ exports.config = {
     }]],
     mochaOpts: {
         ui: 'bdd',
-        timeout: 120000
+        timeout: 180000
     },
     /**
      * Gets executed once before all workers get launched.
