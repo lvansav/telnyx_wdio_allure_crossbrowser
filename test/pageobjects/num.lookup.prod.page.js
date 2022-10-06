b1a929d410d51815daa57c4a52e1f8c5f6cdbbdc
lvansav <vansav.ka@gmail.com>
2022-10-06 14:55:55 +0300

    Update test to the page object pattern

--- test/pageobjects/num.lookup.prod.page.js
+++ test/pageobjects/num.lookup.prod.page.js
@@ -4,6 +4,7 @@ const { Page } = require('./page')
 const aboveSignUpBtn = 'main div:nth-child(4) [href="/sign-up"]'
 const belowContactUsBtn = 'div:nth-child(3) [href*="contact-us"]'
 const tryForFreeBtn = 'div:nth-child(7) [href="/sign-up"]'
+const talkExpertsBelowTryFreeLink = 'div:nth-child(7) [href*="/contact-us"]'
 
 class NumLookupProdPage extends Page {
     async aboveSignUpBtnClick () {
@@ -16,6 +17,10 @@ class NumLookupProdPage extends Page {
 
     async tryForFreeBtnClick () {
         await this.openPage(tryForFreeBtn)
+    }
+
+    async talkExpertsBelowTryFreeLinkClick () {
+        await this.openPage(talkExpertsBelowTryFreeLink)
     }
 }
 
--- test/specs/num.lookup.sign.up.js
+++ test/specs/num.lookup.sign.up.js
@@ -101,57 +101,44 @@ button', async () => {
     it('Should to get talking to an experts from the Number Lookup\
  product page by the "Talk to our experts" link below the "Explore the\
       Docs" button', async () => {
-        const talkExpertsBeloweTryFreeLink = await $('div:nth-child(7) [href*="/contact-us"]')
-        await talkExpertsBeloweTryFreeLink.click()
+        await numLookupProdPage.talkExpertsBelowTryFreeLinkClick()
 
-        const reasonOpts = ['Sales-Inquiry', 'Support', 'Legal']
-        let randReason = reasonOpts[Math.floor(Math.random() * 3)]
+        let randReason = contactUsPage.reasonOpt[Math.floor(Math.random() * 3)]
 
-        if(contact_us_data.reason !== "Support") {
+        if(contact_us_data.reason !== "Support") 
             randReason = contact_us_data.reason
-        }
 
         const randFirstName = faker.name.firstName()
         const randLastName = faker.name.lastName()
         const randEmail = faker.internet.email()
 
-
         const randNumCodeIdx = Math.floor(Math.random() * 221) + 1
-        const randNumCode = await $(`#Phone_Number_Extension__c option:nth-child(${randNumCodeIdx})`).getValue()
+        const randNumCodeElem = await contactUsPage.numCode(randNumCodeIdx)
+        const randNumCode = await randNumCodeElem.getValue()
         
         const randNum = faker.phone.number('###########')
         const randWebSite = faker.internet.url()
         const randText = faker.lorem.paragraph()
+        
+        await contactUsPage.fillInAllFields(
+            randReason,
+            randFirstName,
+            randLastName,
+            randEmail,
+            randNumCode,
+            randNum,
+            randWebSite,
+            randText
+        )
 
-        const reasonSelector = await $('#Reason_for_Contact__c')
-        const firstNameInp = await $('#FirstName')
-        const lastNameInp = await $('#LastName')
-        const emailInp = await $('#Email')
-        const numCodeSelector = await $('#Phone_Number_Extension__c')
-        const numInp = await $('#Phone_Number_Base__c')
-        const webSiteInp = await $('#Website')
-        const addInfoInp = await $('#Form_Additional_Information__c')
-        const receiveBox = await $('[name="Subscription_Opt_In__c"]')
-        const submitBtn = await $('button.mktoButton')
-
-        await reasonSelector.selectByAttribute('value', randReason)
-        await firstNameInp.addValue(randFirstName)
-        await lastNameInp.addValue(randLastName)
-        await emailInp.addValue(randEmail)
-        await numCodeSelector.selectByAttribute('value', randNumCode)
-        await numInp.addValue(randNum)
-        await webSiteInp.addValue(randWebSite)
-        await addInfoInp.addValue(randText)
-        await receiveBox.click()
-
-        await expect(reasonSelector).toHaveValue(randReason)
-        await expect(firstNameInp).toHaveValue(randFirstName)
-        await expect(lastNameInp).toHaveValue(randLastName)
-        await expect(emailInp).toHaveValue(randEmail)
-        await expect(numCodeSelector).toHaveValue(randNumCode)
-        await expect(numInp).toHaveValue(randNum)
-        await expect(webSiteInp).toHaveValue(randWebSite)
-        await expect(addInfoInp).toHaveValue(randText)
-        await expect(submitBtn).toBeClickable()
+        await expect(contactUsPage.reasonSelector()).toHaveValue(randReason)
+        await expect(contactUsPage.firstNameInp()).toHaveValue(randFirstName)
+        await expect(contactUsPage.lastNameInp()).toHaveValue(randLastName)
+        await expect(contactUsPage.emailInp()).toHaveValue(randEmail)
+        await expect(contactUsPage.numCodeSelector()).toHaveValue(randNumCode)
+        await expect(contactUsPage.numInp()).toHaveValue(randNum)
+        await expect(contactUsPage.webSiteInp()).toHaveValue(randWebSite)
+        await expect(contactUsPage.addInfoInp()).toHaveValue(randText)
+        await expect(contactUsPage.submitBtn()).toBeClickable()
     });
 })
